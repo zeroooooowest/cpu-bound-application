6cac9e5aa68a4ec0f3e433e0ef64841c6b8a3f7a
Youngseo Kim <zerowest1001@gmail.com>
2021-05-21 21:25:24 +0900

    initial commit

---
+++ src/main/java/me/zw/cpu/CpuBoundAppApplication.java
@@ -0,0 +1,13 @@
+package me.zw.cpu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpuBoundAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CpuBoundAppApplication.class, args);
+    }
+
+}
